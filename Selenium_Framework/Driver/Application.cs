3da89b1b77f0bea3e85e8f048eda4a0179f4f4a3
murali <muralidaran.palanisamy@aptean.com>
2020-05-07 12:00:14 +0530

    Add project files.

---
+++ Selenium_Framework/Driver/Application.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Framework.Driver
+{
+    public class Application
+    {
+        public void start()
+        {
+            KillProcess("chromedriver");
+            KillProcess("chrome");
+
+        }
+        private void KillProcess(string processName)
+        {
+            Process[] process = Process.GetProcessesByName(processName);
+            foreach (Process pr in process)
+            {
+                try
+                {
+                    pr.Kill();
+                }
+                catch (Exception exe)
+                {
+                    throw exe;
+                    //Console.WriteLine(LogGen.GetExceptionOccurrence(exe.Message));
+                    //Console.WriteLine(LogGen.GetExceptionStackTrace(exe.StackTrace));
+                }
+            }
+        }
+    }
+}
